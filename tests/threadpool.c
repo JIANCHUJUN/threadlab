d6f65aca88144cf5414f46d5efd03d4a26cba76f
Leaves <yjc381818896@gmail.com>
2017-11-05 02:20:30 -0500

    The first version

---
+++ tests/threadpool.c
@@ -0,0 +1,21 @@
+
+static struct deque{
+	void * [] array; // test list
+	int capacity;
+	
+};
+
+struct thread_pool{
+	int number;
+	p_thread[] thread_array;//
+	fork_job_task_t[] task_array;//可能element应该是function和data的二元array？
+};
+
+
+void init_deque(struct deque, int capa)
+{
+	
+}
+
+void add_deque(struct deque, fork_join_task_t)
+
